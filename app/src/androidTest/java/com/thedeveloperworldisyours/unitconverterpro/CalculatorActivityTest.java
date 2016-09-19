fbf3c2f8722015542184480b036a251c2887b781
JavierG <j.cabezas@mundio.com>
2016-09-19 18:16:01 +0100

    added rotatition in calculator test

--- app/src/androidTest/java/com/thedeveloperworldisyours/unitconverterpro/CalculatorActivityTest.java
+++ app/src/androidTest/java/com/thedeveloperworldisyours/unitconverterpro/CalculatorActivityTest.java
@@ -1,6 +1,11 @@
 package com.thedeveloperworldisyours.unitconverterpro;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
 import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.action.ViewActions;
 import android.support.test.rule.ActivityTestRule;
 
@@ -181,5 +186,64 @@ public class CalculatorActivityTest {
         onView(withId(R.id.activity_calculator_result)).check(matches(withText("2")));
         onView(withId(R.id.activity_calculator_equals)).perform(ViewActions.click());
         onView(withId(R.id.activity_calculator_result)).check(matches(withText("4.0")));
+    }
+
+
+    @Test
+    public void testAllOperationTogetherWithRotation() {
+//      Multiply
+        onView(withId(R.id.activity_calculator_two)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("2")));
+        onView(withId(R.id.activity_calculator_multiply)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("2")));
+        onView(withId(R.id.activity_calculator_three)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("3")));
+        onView(withId(R.id.activity_calculator_equals)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("6.0")));
+        rotateScreen();
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("6.0")));
+//      Diminish
+        onView(withId(R.id.activity_calculator_two)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("2")));
+        onView(withId(R.id.activity_calculator_diminish)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("2")));
+        onView(withId(R.id.activity_calculator_three)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("3")));
+        onView(withId(R.id.activity_calculator_equals)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("-1.0")));
+        rotateScreen();
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("-1.0")));
+//      Addition
+        onView(withId(R.id.activity_calculator_two)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("2")));
+        onView(withId(R.id.activity_calculator_plus)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("2")));
+        onView(withId(R.id.activity_calculator_three)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("3")));
+        onView(withId(R.id.activity_calculator_equals)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("5.0")));
+        rotateScreen();
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("5.0")));
+//      Divide
+        onView(withId(R.id.activity_calculator_eight)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("8")));
+        onView(withId(R.id.activity_calculator_divide)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("8")));
+        onView(withId(R.id.activity_calculator_two)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("2")));
+        onView(withId(R.id.activity_calculator_equals)).perform(ViewActions.click());
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("4.0")));
+        rotateScreen();
+        onView(withId(R.id.activity_calculator_result)).check(matches(withText("4.0")));
+    }
+
+    private void rotateScreen() {
+        Context context = InstrumentationRegistry.getTargetContext();
+        int orientation = context.getResources().getConfiguration().orientation;
+
+        Activity activity = mActivityRule.getActivity();
+        activity.setRequestedOrientation(
+                (orientation == Configuration.ORIENTATION_PORTRAIT) ?
+                        ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE : ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
     }
 }
